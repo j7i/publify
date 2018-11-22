66500ab2d0aa43eda5e5a766c1a84e265c461224
joWme <joe.willi@icloud.com>
2018-11-23 00:37:16 +0100

    Resolve module in login page correctly

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,5 @@
+import SignIn from '@auth/signIn'
 import ErrorBoundary from '@helpers/errorBoundary'
-import SignIn from 'components/@auth/signIn'
 import { PureComponent } from 'react'
 
 export default class Login extends PureComponent {
